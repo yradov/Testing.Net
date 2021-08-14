853d08cc77bf1597f14aab12aadb4faf62557a19
RYuriy <radov.yuriy@ukr.net>
2021-08-14 16:01:49 +0300

    + Product Model

---
+++ SimpleApp/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleApp.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+        public static Product[] GetProducts()
+        {
+            Product kayak = new()
+            { Name = "Kayak", Price = 275M };
+
+            Product lifejacket = new() { Name = "Lifejacket", Price = 48.95M };
+
+            return new Product[] { kayak, lifejacket };
+        }
+    }
+}
