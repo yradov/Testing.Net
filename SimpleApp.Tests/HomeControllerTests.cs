afc9300e2bf6c76d28dee7a913ff90b72ceb6473
RYuriy <radov.yuriy@ukr.net>
2021-08-16 00:48:48 +0300

    + custom Comparer class and HomeController tests

---
+++ SimpleApp.Tests/HomeControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SimpleApp.Controllers;
+using SimpleApp.Models;
+using Xunit;
+
+
+namespace SimpleApp.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexActionModelIsComplete()
+        {
+            var controller = new HomeController();
+            Product[] products = new Product[] {
+                new Product { Name = "Kayak", Price = 275M },
+                new Product { Name = "Lifejacket", Price = 48.95M }
+            };
+
+            var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+
+            Assert.Equal(expected: products,
+                         actual: model,
+                         comparer: Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
+        }
+    }
+}
